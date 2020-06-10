b637433626d903a93d9bc8851640d92b3852b3a5
Alex Robertson <arobertson335@gmail.com>
2020-06-10 16:09:33 -0700

    Added column to order items for status.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_230228) do
+ActiveRecord::Schema.define(version: 2020_06_10_230811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_230228) do
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "orders", force: :cascade do |t|
