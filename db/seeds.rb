557999362bbae8d021dd7a763cd59215c8383f57
Alex Robertson <arobertson335@gmail.com>
2020-06-11 14:20:34 -0700

    Add files for seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+MERCHANT_FILE = Rails.root.join('db', 'seed_data', 'merchants.csv')
+puts "Loading raw driver data from #{MERCHANT_FILE}"
+
+merchant_failures = []
+CSV.foreach(MERCHANT_FILE, :headers => true) do |row|
+  merchant = Merchant.new
+  merchant.id = row['id']
+  merchant.username = row['username']
+  merchant.email = row['email']
+  successful = merchant.save
+  if !successful
+    merchant_failures << merchant
+    puts "Failed to save merchant: #{merchant.inspect}"
+  else
+    puts "Created merchant: #{merchant.inspect}"
+  end
+end
+
+puts "Added #{Merchant.count} merchant records"
+puts "#{merchant_failures.length} merchants failed to save"
+
+PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products.csv')
+puts "Loading raw product data from #{PRODUCT_FILE}"
+
+product_failures = []
+CSV.foreach(PRODUCT_FILE, :headers => true) do |row|
+  product = Product.new
+  product.name = row['name']
+  product.id = row['id'] 
+  product.merchant_id = row['merchant_id']
+  product.inventory = row['inventory']
+  product.price = row['price']
+  product.description = row['description']
+  product.img_url = row['img_url']
+  product.active = row['active']
+  successful = product.save
+  if !successful
+    product_failures << product
+    puts "Failed to save products: #{product.inspect}"
+  else
+    puts "Created products: #{product.inspect}"
+  end
+end
+
+puts "Added #{Product.count} product records"
+puts "#{product_failures.length} products failed to save"
+
+ActiveRecord::Base.connection.tables.each do |t|                   
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
