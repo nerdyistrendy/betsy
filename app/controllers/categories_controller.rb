9d75736c7f4e5e851ac532715f89c7312b3fd3ec
cojenco <icathyouyang@gmail.com>
2020-06-12 14:19:22 -0700

    add categories#create controller action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,23 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+  
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:success] = "Successfully created #{@category.name} ID# #{@category.id}"
+      redirect_back(fallback_location: products_path)
+      return
+    else
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def category_params
+    return params.require(:category).permit(:name)
+  end
 end
