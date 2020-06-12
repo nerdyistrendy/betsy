c865459b71c2a95716b50f557f3ec92d4886dabb
cojenco <icathyouyang@gmail.com>
2020-06-11 18:42:42 -0700

    add merchants#create action for OAuth login

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,28 +1,26 @@
 class MerchantsController < ApplicationController
   def create
-    redirect_to products_path
     # auth_hash = request.env["omniauth.auth"]
-    # user = Merchant.find_by(uid: auth_hash[:uid], provider: auth_hash[:provider])
-    # if user         # User was found in the database
-    #   flash[:status] = :success
-    #   flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    # else
-    #   # User doesn't match anything in the DB
-    #   user = Merchant.build_from_github(auth_hash)
+    # raise
+    # redirect_to products_path
+    auth_hash = request.env["omniauth.auth"]
+    merchant = Merchant.find_by(email: auth_hash["info"]["email"])
+    if merchant         # Merchant was found in the database
+      flash[:success] = "Successfully logged in as existing user #{merchant.username}"
+    else
+      # Merchant doesn't match anything in the DB
+      merchant = Merchant.build_from_github(auth_hash)
 
-    #   if user.save
-    #     flash[:status] = :success
-    #     flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-    #   else
-    #     flash[:status] = :failure
-    #     flash[:result_text] = "Could not log in"
-    #     flash[:messages] = user.errors.messages
-    #     redirect_to root_path
-    #   end
-    # end
+      if merchant.save
+        flash[:success] = "Successfully created new user #{merchant.username} with ID #{merchant.id}"
+      else
+        flash[:failure] = merchant.errors.messages
+        redirect_to products_path
+      end
+    end
     
-    # # If we get here, we have a valid user instance
-    # session[:user_id] = user.id
-    # redirect_to root_path
+    # If we get here, we have a valid user instance
+    session[:user_id] = merchant.id
+    redirect_to products_path
   end
 end
