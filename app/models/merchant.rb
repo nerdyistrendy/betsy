6839e121d0bd9b5bad7c6a4c9d79acb929c2c547
cojenco <icathyouyang@gmail.com>
2020-06-11 18:41:42 -0700

    add class method build_from_github in Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord 
   has_many :order_items
   has_many :products
+
+  def self.build_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.username = auth_hash["info"]["nickname"]
+    merchant.email = auth_hash["info"]["email"]
+
+    # The user is not saved here. Validate and save in controller.
+    return merchant
+  end
 end
