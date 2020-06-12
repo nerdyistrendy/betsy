b6a2aa7f5b51da0a4963e9b823fe3db4032d3650
cojenco <icathyouyang@gmail.com>
2020-06-12 11:00:01 -0700

    change redirect routes to root path

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -12,18 +12,18 @@ class MerchantsController < ApplicationController
         flash[:success] = "Successfully created new user #{merchant.username} with ID #{merchant.id}"
       else
         flash[:failure] = merchant.errors.messages
-        redirect_to products_path
+        redirect_to root_path
       end
     end
     
     # If we get here, we have a valid user instance
     session[:user_id] = merchant.id
-    redirect_to products_path
+    redirect_to root_path
   end
 
   def logout
     session[:user_id] = nil
     flash[:success] = "Successfully logged out"
-    redirect_to products_path
+    redirect_to root_path
   end
 end
