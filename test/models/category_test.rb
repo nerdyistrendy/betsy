5e363b75a41402c67ef2068ad1dc026f0d15e148
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-13 14:52:54 -0700

    Fixed merge conflict on category_test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -6,24 +6,6 @@ describe Category do
     @pickle_test = products(:pickles)
   end
   
-  describe "active_products" do
-    it "will retrieve all products where active is true" do
-      products_arr = @food_test.active_products
-
-      expect(products_arr).must_include @pickle_test
-      products_arr.each do |p|
-        expect(p.active).must_equal true
-      end
-    end
-
-    it "will ignore inactive products" do
-      inactive_pickle = products(:inactive_pickles)
-      products_arr = @food_test.active_products
-
-      expect(products_arr).wont_include inactive_pickle
-    end
-  end
-
   describe "validations" do
     before do
       @new_cat = Category.new(name: "Spiritual Support")
@@ -72,6 +54,24 @@ describe Category do
       @lifestyle.products << @pickle_test
       expect(@lifestyle.products).must_include @pickle_test
       expect(@pickle_test.categories).must_include @lifestyle
+    end
+  end
+
+  describe "active_products" do
+    it "will retrieve all products where active is true" do
+      products_arr = @food_test.active_products
+
+      expect(products_arr).must_include @pickle_test
+      products_arr.each do |p|
+        expect(p.active).must_equal true
+      end
+    end
+
+    it "will ignore inactive products" do
+      inactive_pickle = products(:inactive_pickles)
+      products_arr = @food_test.active_products
+
+      expect(products_arr).wont_include inactive_pickle
     end
   end
 end
