1e79b3de99fb6da4e40671e934a8ff6af0decb37
Alex Robertson <arobertson335@gmail.com>
2020-06-12 19:20:01 -0700

    Adds fixtures for merchants tests.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,8 +2,5 @@ class Merchant < ApplicationRecord
   has_many :order_items
   has_many :products
   has_many :orders
-  
-  def authorized? 
-    # TODO: Add in functionality to check if merchant is logged in. RE: merchants/show.html.erb file
-  end 
+
 end
--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,6 +1,36 @@
 require "test_helper"
 
-describe MerchantsController do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  describe "show" do 
+    it "successfully renders the show page for a merchant's dashboard" do
+      get merchant_path()
+
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
