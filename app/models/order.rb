a90bba90891eb3e675dcd68c11b640043fce5929
Yieni <yaynee8d@gmail.com>
2020-06-14 00:44:13 -0700

    add dropdown for expiration date. add validations on order controller for order form fields

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ApplicationRecord
   has_many :order_items
-
-
+  validates :name, :email, :mailing_address, :cc_name, :cc_cvv, :cc_exp, :cc_number, :zipcode, :status, presence: true
+  validates :email, format: { with: /\w+@\w+\.\w+/, message: "Please enter valid email address" }
+  validates :cc_cvv, :cc_exp, :cc_number, :zipcode, numericality: { only_integer: true }
+  validates :cc_number, length: {is: 15}
+  validates :cc_cvv, length: {is: 3}
+  validates :zipcode, length: {is: 5}
 end
