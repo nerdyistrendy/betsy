74a8017d3c1c611109b28fd2098c479b5cb8d90c
cojenco <icathyouyang@gmail.com>
2020-06-14 20:22:04 -0700

    revise order_include_merchant to an Order model method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,26 @@
 class OrdersController < ApplicationController
   before_action :require_login, only: [:show]
   before_action :get_order, only: [:show]
-  before_action :order_include_merchant, only: [:show]
   
   def cart
     @session = session
   end
 
   def show
-    # require_login controller filter
-    # order_include_merchant controller filter: checking if the merchant is authorized to view this page
-    if !@order
+    # require_login controller filter: only authorized merchant can view
+    if @order
+      if @order.order_include_merchant(@login_merchant)    # order includes products from @login_merchant 
+        @items_from_merchant = @order.order_include_merchant(@login_merchant)
+        return
+      else                                                 # order does NOT include product from @login_merchant 
+        flash[:warning] = "You are not authorized to view this section"
+        redirect_back(fallback_location: root_path)
+        return
+      end
+    else
       flash[:warning] = "Invalid Order"
-      redirect_to root_path
+      redirect_back(fallback_location: root_path)
+      return
     end
   end
 
@@ -20,27 +28,5 @@ class OrdersController < ApplicationController
 
   def get_order
     return @order = Order.find_by(id: params[:id])
-  end
-
-  # might need to break into 1 model method and 1 controller filter.....
-  def order_include_merchant
-   
-    # @items_from_merchant = @order.order_items.where(merchant_id: @login_merchant.id)
-
-    @items_from_merchant = []
-
-    if @order
-      @order.order_items.each do |item|
-        @items_from_merchant << item if item.merchant_id == @login_merchant.id
-      end
-
-      if @items_from_merchant.empty?
-        flash[:warning] = "You are not authorized to view this section"
-        redirect_to root_path
-        return false
-      else
-        return @items_from_merchant
-      end
-    end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,15 @@
 class Order < ApplicationRecord
   has_many :order_items
 
+  # if order instance includes order_items from this merchant
+  def order_include_merchant(merchant)
+    items_from_merchant = self.order_items.where(merchant_id: merchant.id)
+
+    if items_from_merchant.empty?
+      return false
+    else
+      return items_from_merchant
+    end
+  end
 
 end
