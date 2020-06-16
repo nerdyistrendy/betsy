48bfae7d519d9db84162f4ae3d038af60b64b4c0
cojenco <icathyouyang@gmail.com>
2020-06-15 18:09:23 -0700

    add #subtotal OrderItem model method and tests

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,10 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
 
-  
+  def subtotal
+    product = Product.find_by(id: self.product_id)
+    if self.quantity && product
+      return (self.quantity * product.price).to_f.round(2)
+    end
+  end
 end
