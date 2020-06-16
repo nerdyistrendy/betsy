a7b5db6362ee629101909e1e13b4a0e8be7de71b
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-15 18:44:59 -0700

    Added validations for review model and tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :text, presence: true, length: { in: 1..1000 }
+  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :reviewer, presence: true, length: { in: 1..30 }
+  validates_associated :product
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -5,10 +5,57 @@ describe Review do
     @excitable_review_test = reviews(:excitable_pickle_review)
     @pickle_test = products(:pickles)
     @blacksmith_test = merchants(:blacksmith)
-    @confused_review_test = products(:confused_pickle_review:)
+    @confused_review_test = reviews(:confused_pickle_review)
   end
 
   describe "validations" do
+    it "is valid when all fields are present" do
+      expect(@excitable_review_test.valid?).must_equal true
+    end
 
+    it "is invalid when missing or invalid text" do
+      invalid_excitable_review = @excitable_review_test
+      invalid_excitable_review.text = nil  
+
+      invalid_confused_review = @confused_review_test
+      invalid_confused_review.text = ""
+
+      expect(invalid_excitable_review.valid?).must_equal false
+      expect(invalid_confused_review.valid?).must_equal false
+    end
+
+    it "is invalid when missing or invalid rating" do
+      invalid_excitable_review = @excitable_review_test
+      invalid_excitable_review.rating = nil  
+
+      invalid_confused_review = @confused_review_test
+      invalid_confused_review.rating = "5"
+
+      expect(invalid_excitable_review.valid?).must_equal false
+      expect(invalid_confused_review.valid?).must_equal false
+    end
+
+    it "is invalid when missing or invalid reviewer" do
+      invalid_excitable_review = @excitable_review_test
+      invalid_excitable_review.reviewer = nil  
+
+      invalid_confused_review = @confused_review_test
+      invalid_confused_review.reviewer = ""
+
+      expect(invalid_excitable_review.valid?).must_equal false
+      expect(invalid_confused_review.valid?).must_equal false
+    end
+  end
+
+  describe "relations" do
+    describe "products" do
+      it "can have a single product" do
+
+      end
+
+      it "is invalid when it doesn't belong to a product" do
+
+      end
+    end
   end
 end
