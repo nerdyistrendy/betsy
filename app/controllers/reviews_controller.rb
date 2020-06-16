b088d5f5b56963275c524f6925bfc7973512972c
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-15 19:16:35 -0700

    Added reviews#new action and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @product = params[:product_id]
+    @review = Review.new
+  end
+
+  def create
+  end
+
+  private
+
+  def review_params
+    return params.require(:review).permit(:reviewer, :text, :rating, :product_id)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
 
-  validates :text, presence: true, length: { in: 1..1000 }
+  validates :text, presence: true, length: { in: 4..1000 }
   validates :rating, presence: true, numericality: { only_integer: true }
   validates :reviewer, presence: true, length: { in: 1..30 }
   validates_associated :product
