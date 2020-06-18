52b525cae30c28cadff7dcfa9769e390345337ab
cojenco <icathyouyang@gmail.com>
2020-06-17 19:39:24 -0700

    revise orderitems#fulfillment tests

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -8,22 +8,15 @@ describe OrderItemsController do
   end
 
   describe "Guest Users" do
-    describe "index" do
+    describe "fulfillment" do
       it "cannot access the order_items#index and will be redirected" do
-        get merchant_order_items_path(@merchant_test)
+        get fulfillment_path
         must_respond_with :redirect
         expect(flash[:warning]).must_equal "You must be logged in to view this section"
       end
 
       it "cannot access the order_items#index with string query and will be redirected" do
-        get merchant_order_items_path(@merchant_test, :status => "pending")
-        must_respond_with :redirect
-        expect(flash[:warning]).must_equal "You must be logged in to view this section"
-      end
-
-      it "redirects for an invalid search" do
-        @invalid_order_item_id = -5
-        get merchant_order_items_path(@invalid_order_item_id)
+        get fulfillment_path(:status => "pending")
         must_respond_with :redirect
         expect(flash[:warning]).must_equal "You must be logged in to view this section"
       end
@@ -48,36 +41,55 @@ describe OrderItemsController do
   end
 
   describe "Logged in Merchants" do
-    describe "index" do
+    # describe "index" do
+    #   before do
+    #     perform_login(@merchant_test)
+    #   end
+
+    #   it "returns the index of order items for a given merchant" do
+    #     get merchant_order_items_path(@merchant_test.id)
+    #     must_respond_with :success
+    #   end 
+
+    #   it "returns the index of order items by status when passed in a string query" do
+    #     get merchant_order_items_path(@merchant_test, :status => "pending")
+    #     must_respond_with :success
+
+    #     get merchant_order_items_path(@merchant_test, :status => "shipped")
+    #     must_respond_with :success
+    #   end 
+
+    #   it "redirects if the fulfillment page does not belong to the logged in merchant" do        
+    #     get merchant_order_items_path(@weavery)
+    #     must_respond_with :redirect
+    #     expect(flash[:warning]).must_equal "You are not authorized to view this section"
+    #   end
+      
+    #   it "redirects for an invalid search" do
+    #     @invalid_order_item_id = -5
+    #     get merchant_order_items_path(@invalid_order_item_id)
+    #     must_respond_with :redirect
+    #     expect(flash[:warning]).must_equal "You are not authorized to view this section"
+    #   end
+    # end
+
+    describe "fulfillment (order fulfillment page)" do
       before do
         perform_login(@merchant_test)
       end
 
-      it "returns the index of order items for a given merchant" do
-        get merchant_order_items_path(@merchant_test.id)
+      it "returns the order items for a given merchant" do
+        get fulfillment_path
         must_respond_with :success
       end 
 
-      it "returns the index of order items by status when passed in a string query" do
-        get merchant_order_items_path(@merchant_test, :status => "pending")
+      it "returns the order items by status when passed in a string query" do
+        get fulfillment_path(:status => "pending")
         must_respond_with :success
 
-        get merchant_order_items_path(@merchant_test, :status => "shipped")
+        get fulfillment_path(:status => "shipped")
         must_respond_with :success
       end 
-
-      it "redirects if the fulfillment page does not belong to the logged in merchant" do        
-        get merchant_order_items_path(@weavery)
-        must_respond_with :redirect
-        expect(flash[:warning]).must_equal "You are not authorized to view this section"
-      end
-      
-      it "redirects for an invalid search" do
-        @invalid_order_item_id = -5
-        get merchant_order_items_path(@invalid_order_item_id)
-        must_respond_with :redirect
-        expect(flash[:warning]).must_equal "You are not authorized to view this section"
-      end
     end
 
     describe "ship" do
