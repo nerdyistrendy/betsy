90ac019d1ad4105b55447d4fcddbbff448127055
cojenco <icathyouyang@gmail.com>
2020-06-17 22:08:53 -0700

    move business logic to OrderItem model class method filter_merchant_status and add tests

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -9,4 +9,13 @@ class OrderItem < ApplicationRecord
       return (self.quantity * product.price).to_f.round(2)
     end
   end
+
+  def self.filter_merchant_status(merchant: nil, status: nil)
+    return [] if merchant.nil?
+    if status
+      return OrderItem.where(merchant_id: merchant.id, status: status)
+    else
+      return OrderItem.where(merchant_id: merchant.id)
+    end
+  end
 end
--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -41,38 +41,6 @@ describe OrderItemsController do
   end
 
   describe "Logged in Merchants" do
-    # describe "index" do
-    #   before do
-    #     perform_login(@merchant_test)
-    #   end
-
-    #   it "returns the index of order items for a given merchant" do
-    #     get merchant_order_items_path(@merchant_test.id)
-    #     must_respond_with :success
-    #   end 
-
-    #   it "returns the index of order items by status when passed in a string query" do
-    #     get merchant_order_items_path(@merchant_test, :status => "pending")
-    #     must_respond_with :success
-
-    #     get merchant_order_items_path(@merchant_test, :status => "shipped")
-    #     must_respond_with :success
-    #   end 
-
-    #   it "redirects if the fulfillment page does not belong to the logged in merchant" do        
-    #     get merchant_order_items_path(@weavery)
-    #     must_respond_with :redirect
-    #     expect(flash[:warning]).must_equal "You are not authorized to view this section"
-    #   end
-      
-    #   it "redirects for an invalid search" do
-    #     @invalid_order_item_id = -5
-    #     get merchant_order_items_path(@invalid_order_item_id)
-    #     must_respond_with :redirect
-    #     expect(flash[:warning]).must_equal "You are not authorized to view this section"
-    #   end
-    # end
-
     describe "fulfillment (order fulfillment page)" do
       before do
         perform_login(@merchant_test)
