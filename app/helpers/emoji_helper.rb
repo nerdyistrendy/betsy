0da1289366d3a9105ad54fd4d3e370c9de1bdb82
Alicia Combs <alicia.combs.92@gmail.com>
2020-06-18 18:20:20 -0700

    Added documentation to view helper

--- app/helpers/emoji_helper.rb
+++ app/helpers/emoji_helper.rb
@@ -5,3 +5,11 @@ module EmojiHelper
     Twemoji.parse(h(content), options).html_safe if content.present?
   end
 end
+
+# Used in erb using keywords from
+# https://github.com/jollygoodcode/emoji-keywords
+# Using this this syntax:
+
+# <p><%= emojify "I like chocolate :heart_eyes:!" %></p>
+# would render:
+# <p>I like chocolate <img class="emoji" draggable="false" title=":heart_eyes:" alt="😍" src="https://twemoji.maxcdn.com/2/72x72/1f60d.png">!</p>
